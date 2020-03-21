0591647986494e98fb46e25a4aea907b8baff19e
shubham <shub2495@gmail.com>
2020-03-21 12:13:36 +0530

    Minor Changes

--- text_on_image.php
+++ text_on_image.php
@@ -1,18 +1,19 @@
 <?php
+    error_reporting(0);
+    // Output Image
+    header('Content-type: image/jpeg');
     // Provide Image Path
     $img = imagecreatefromjpeg('image.jpg');
     // Text Color Of the Image
     $white = imagecolorallocate($img, 255, 255, 255);
     // Text On the image, This can be fetch from the database.
-    $txt = "Hello World";
+    $txt = "ABC-1234";
     // Google fonts can be include or system fonts can be taken by default
     $font = "C:\Windows\Fonts\arial.ttf"; 
     // Specify the coordinates on the image and size of the image
     imagettftext($img, 40, 0, 300, 300, $white, $font, $txt);
-    // Output Image
-    header('Content-type: image/jpeg');
     //Image will display on the browser
-    imagejpeg($img);  
-    // Render the Image to the path
-    $img->render('destination.jpg');   
+    $name = rand();
+    imagejpeg($img,"photos/".$name.".jpg");    
+    imagedestroy($img);
 ?>
