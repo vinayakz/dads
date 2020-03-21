fbae6ef20d91183d2b14d301591c38d73e99de2d
shubham <shub2495@gmail.com>
2020-03-21 13:02:46 +0530

    Create PDF From Image

--- text_on_image.php
+++ text_on_image.php
@@ -1,5 +1,7 @@
 <?php
     error_reporting(0);
+    //include this php file
+    require('fpdf/fpdf.php');    
     // Output Image
     header('Content-type: image/jpeg');
     // Provide Image Path
@@ -16,4 +18,12 @@
     $name = rand();
     imagejpeg($img,"photos/".$name.".jpg");    
     imagedestroy($img);
+    //Create Object of FPDF File
+    $pdf = new FPDF(); 
+    //Add Page to PDF File
+    $pdf->AddPage();
+    //Provide Correct path and name of the  image 0,0 is a X & Y coordinate and 250 is the size of the image in pdf
+    $pdf->Image('photos/'.$name.'.jpg',0,0,210);
+    //Output will start downloading at clients Browser
+    $pdf->Output();
 ?>
